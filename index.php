23dd3cf12172b3845145e4d5e7872135b6e0d311
Gilson <gilson@idnadevendas.com.br>
2015-01-21 22:34:23 -0200

    Classe Cliente, Listagem de Cliente e Ordenação

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include_once("./pages/layout/header_padrao.php"); ?>
+
+<?php include __DIR__ . "/admin/cliente/clienteDAO.php"; ?>
+
+<?php include_once("./pages/layout/menu.php"); ?>
+
+<?php include_once("./pages/layout/footer.php"); ?>
